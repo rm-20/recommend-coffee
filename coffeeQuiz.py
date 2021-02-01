46efab462437d97c068e034cd2918570bc882f20
Mehta <roma.mehta@definitivelogic.com>
2021-01-31 22:02:02 -0500

    Added lists of coffee recommendations, will be able to add future questions if needed.

--- coffeeQuiz.py
+++ coffeeQuiz.py
@@ -1,185 +1,154 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-
-"""
 import tkinter as tk
 from tkinter import *
 import random
-import questionsAnswers
-
+import sqlite3
 class NewApp:
-    def __init__(self):
+    def __init__(self): 
        self.window = tk.Tk()
        self.window.geometry("500x500")
        welcome_btn = Button(self.window, text = "Welcome", command = self.newPage)
        welcome_btn.grid(row = 0, column = 0, sticky = W, pady = 2)
-       self.window.mainloop()
-       
+       self.window.mainloop()      
     def newPage(self):
         self.newWindow = Toplevel(self.window)
         self.newWindow.geometry("500x500")
         title = Label(self.newWindow,text = "Please answer the questions below")
-        title.grid(column = 0, row = 1, sticky = W, pady = 2)
+        title.grid(column = 0, row = 0, sticky = W, pady = 2)
+        self.create_DB()
         self.group1 = 0
         self.group2 = 0
         self.group3 = 0
         self.group4 = 0
-        
-        q1 = Label(self.newWindow, text = "What's your favorite season?")
-        q1.grid(column = 0, row = 2,sticky = W, pady = 2)
-        
-        self.fall = Button(self.newWindow, text = "Fall", command = self.storeFall)
-        self.fall.grid(column = 0, row = 3, sticky = W, pady = 2)
-        
-        self.spring = Button(self.newWindow, text = "Spring", command = self.storeSpring)
-        self.spring.grid(column = 0, row = 4, sticky = W, pady = 2)
-        
-        self.summer = Button(self.newWindow, text = "Summer", command = self.storeSummer)
-        self.summer.grid(column = 0, row = 5, sticky = W, pady = 2)
-        
-        self.winter = Button(self.newWindow, text = "Winter", command = self.storeWinter)
-        self.winter.grid(column = 0, row = 6, sticky = W, pady = 2)
-        
-        q2 = Label(self.newWindow, text = "What's your favorite show?")
-        q2.grid(column = 0, row = 8, sticky = W, pady = 2)
-        
-        self.show1 = Button(self.newWindow, text = "Avatar: The Last Airbender", command = self.storeShow1)
-        self.show1.grid(column = 0, row = 9, sticky = W, pady = 2)
-        
-        self.show2 = Button(self.newWindow, text = "The Office", command = self.storeShow2)
-        self.show2.grid(column = 0, row = 10, sticky = W, pady = 2)
-        
-        self.show3 = Button(self.newWindow, text = "One Tree Hill", command = self.storeShow3)
-        self.show3.grid(column = 0, row = 11, sticky = W, pady = 2)
-        
-        self.show4 = Button(self.newWindow, text = "Wizards of Waverly Place", command = self.storeShow4)
-        self.show4.grid(column = 0, row = 12, sticky = W, pady = 2)
-        
-        q3 = Label(self.newWindow, text = "What's your favorite book genre?")   
-        q3.grid(column = 0, row = 14, sticky = W, pady = 2)
-       
-        self.g1 = Button(self.newWindow, text = "horror", command = self.storeG1)
-        self.g1.grid(column = 0, row = 15, sticky = W, pady = 2)
-        
-        self.g2 = Button(self.newWindow, text = "comedy", command = self.storeG2)
-        self.g2.grid(column = 0, row = 16, sticky = W, pady = 2)
-        
-        self.g3 = Button(self.newWindow, text = "mystery", command = self.storeG3)
-        self.g3.grid(column = 0, row = 17, sticky = W, pady = 2)
-        
-        self.g4 = Button(self.newWindow, text = "romance", command = self.storeG4)
-        self.g4.grid(column = 0, row = 18, sticky = W, pady = 2)      
-       
-        self.submit = Button(self.newWindow, text = "Done!", command = self.quizResult)
-        self.submit.grid(column = 0, row = 20, sticky = W, pady = 2)
-        
-    def storeFall(self):
+        self.result = []     
+    def create_DB(self):
+        conn = sqlite3.connect('example.db')
+        c = conn.cursor()
+        #c.execute("""CREATE TABLE questionAnswer (id integer primary key not null,
+         #         question text, group1 text, group2 text, group3 text, group4 text)""")
+        question_list = ['Pick your favorite show','Pick your favorite genre','Pick your favorite character','Pick your favorite past time']
+        group1_list = ['Wizards of Waverly Place','Horror','Hermione Granger','Reading']
+        group2_list = ['One Tree Hill','Drama','Bella Swan','Exercising']
+        group3_list = ['The Office','Comedy','Kelly Kapoor','Cooking']
+        group4_list = ['Avatar:The Last Airbender','Adventure','Toph Beifong','Exploring']
+        conn.commit()
+        ##First row 
+        #c.execute("""INSERT INTO questionAnswer VALUES(1,'Whats your favorite season','Fall','Winter','Spring','Summer')""")
+        #Starting column 2 at row 2
+        #row = 1
+        #for item in question_list:
+        #     row += 1
+        #     c.execute("INSERT INTO questionAnswer(question) VALUES(?)",(item,))
+        #conn.commit()
+        ##Col 3
+        #row = 1
+        #for item in group1_list:
+        #    row += 1
+        #    c.execute("UPDATE questionAnswer SET group1 = ? WHERE id = ?",(item,row))
+        #conn.commit()
+        ##Col 4
+        #row = 1
+        #for item in group2_list:
+        #   row += 1
+        #   c.execute("UPDATE questionAnswer SET group2 = ? WHERE id = ?",(item,row))
+        #conn.commit()
+        ##Col 5
+        #row = 1
+        #for item in group3_list:
+        #    row += 1
+        #    c.execute("UPDATE questionAnswer SET group3 = ? WHERE id = ?",(item,row))
+        #conn.commit()
+        ##Col 6
+        #row = 1
+        #for item in group4_list:
+        #    row += 1
+        #    c.execute("UPDATE questionAnswer SET group4 = ? WHERE id = ?",(item,row))
+        #conn.commit()
+        #for row in c.execute("SELECT * FROM questionAnswer"):
+        #    print(row)              
+        rw = 0
+        col = 0
+        x = 0
+        for row in c.execute("SELECT id,question FROM questionAnswer"):
+            col+= 1 
+            rw += 1
+            x += 1
+            q_label = Label(self.newWindow, text = row)
+            q_label.grid(column = 0, row = rw, sticky = W, pady = 2)
+        new_rw = 0
+        for row in c.execute("SELECT group1 FROM questionAnswer"):
+            new_rw += 1
+            self.g1btn = Button(self.newWindow, text = row, command = self.g1Counter)
+            self.g1btn.grid(column = 1, row = new_rw,sticky = W, pady = 2)
+        new_rw = 0
+        for row in c.execute("SELECT group2 FROM questionAnswer"):
+            new_rw += 1
+            self.g2btn = Button(self.newWindow, text = row, command = self.g2Counter)
+            self.g2btn.grid(column = 2, row = new_rw,sticky = W, pady = 2)
+        new_rw = 0
+        for row in c.execute("SELECT group3 FROM questionAnswer"):
+            new_rw += 1
+            self.g3btn = Button(self.newWindow, text = row, command = self.g3Counter)
+            self.g3btn.grid(column = 3, row = new_rw,sticky = W, pady = 2)
+        new_rw = 0
+        for row in c.execute("SELECT group4 FROM questionAnswer"):
+            new_rw += 1
+            self.g4btn = Button(self.newWindow, text = row, command = self.g3Counter)
+            self.g4btn.grid(column = 4, row = new_rw,sticky = W, pady = 2)
+        submit = Button(self.newWindow, text = "Submit", command = self.submit)
+        submit.grid(column = 0, row = 7, sticky = W, pady = 2)
+        self.g1 = ['Caramel Machiatto', 'Pumpkin Spice Latte','Chestnut Praline Latte','Capuccino with Cinnamon']
+        self.g2 = ['Gingerbread Latte', 'Peppermint Mocha','Cafe Mocha','Spiced Vanilla Latte']
+        self.g3 = ['Rose Coffee','Iced Vietnamese Latte','Vanilla Iced Latte','Cafe Con Miel']
+        self.g4 = ['Cold Coffee','Turkish Coffee','Iced Thai Latte','Nutella Iced Coffee']
+        self.rand_coffees = ['Flat White','Black Coffee','Espresso','Plain Latte','Plain Cappuccino','Cafe Au Lait','Americano']
+        list_array = [self.g1, self.g2,self.g3,self.g4,self.rand_coffees]
+    def g1Counter(self):
         self.group1 += 1
-        print(self.group1)
-
-    def storeSpring(self):
-        self.group3 += 1
-        print(self.group3)
-    
-    def storeSummer(self):
-        self.group4 += 1
-        print(self.group4)
-    
-    def storeWinter(self):
+        if self.group1 >= 3:
+            self.result = self.g1
+    def g2Counter(self):
         self.group2 += 1
-        print(self.group2)
-        
-    def storeShow1(self):
-        self.group4 += 1
-        print(self.group4)
-    
-    def storeShow2(self):
+        if self.group2 >= 3:
+            self.result = self.g2
+    def g3Counter(self):
         self.group3 += 1
-        print(self.group3)
-    
-    def storeShow3(self):
-        self.group2 += 1
-        print(self.group2)
-    
-    def storeShow4(self):
-        self.group1 += 1
-        print(self.group1)
-        
-    def storeG1(self):
-        self.group1 += 1
-        print(self.group1)    
-        
-    def storeG2(self):
-        self.group2 += 1
-        print(self.group2)    
-        
-    def storeG3(self):
-        self.group3 += 1
-        print(self.group3)
-        
-    def storeG4(self):
+        if self.group3 >= 3:
+            self.result = self.g3
+    def g4Counter(self):
         self.group4 += 1
-        print(self.group4)
-    
-    def quizResult(self):
-        if self.group1 >= 2:
-            group_1 = ['Pumpkin Spice Latte','Cinnamon Maple Latte','Caramel Macchiato','Chestnut Praline Latte','Cafe Mocha']
-            rec = Label(self.newWindow, text = random.choice(group_1))
-            rec.grid(column = 2, row = 1, sticky = W, pady = 2)
-            thank_you = Button(self.newWindow, text = "Thank you!", command = self.newWindow.destroy)
-            thank_you.grid(column = 2, row = 2, sticky = W, pady = 2)
-            try_again = Button(self.window, text = "Try again", command = self.newPage)
-            try_again.grid(column = 0, row = 2, sticky = W, pady = 2)
-            good_bye = Button(self.window, text = "Goodbye", command = self.window.destroy)
-            good_bye.grid(column = 0, row = 3, sticky = W, pady = 2)
-            
-            
-        elif self.group2 >= 2:
-            group_2 = ['Peppermint Mocha','Gingerbread Latte','Winter Spiced Coffee','Cappuccino']
-            rec = Label(self.newWindow, text = random.choice(group_2))
-            rec.grid(column = 2, row = 1, sticky = W, pady = 2)
-            thank_you = Button(self.newWindow, text = "Thank you!", command = self.newWindow.destroy)
-            thank_you.grid(column = 2, row = 2, sticky = W, pady = 2)
-            try_again = Button(self.window, text = "Try again", command = self.newPage)
-            try_again.grid(column = 0, row = 2, sticky = W, pady = 2)
-            good_bye = Button(self.window, text = "Goodbye", command = self.window.destroy)
-            good_bye.grid(column = 0, row = 3, sticky = W, pady = 2)
-            
-        elif self.group3 >= 2:
-            group_3 = ['Thai Iced Coffee','Cafe Con Miel', 'Rose Coffee','Iced Vietnamese Latte']
-            rec = Label(self.newWindow, text = random.choice(group_3))
-            rec.grid(column = 2, row = 1, sticky = W, pady = 2)
-            thank_you = Button(self.newWindow, text = "Thank you!", command = self.newWindow.destroy)
-            thank_you.grid(column = 2, row = 2, sticky = W, pady = 2)
-            try_again = Button(self.window, text = "Try again", command = self.newPage)
-            try_again.grid(column = 0, row = 2, sticky = W, pady = 2)
-            good_bye = Button(self.window, text = "Goodbye", command = self.window.destroy)
-            good_bye.grid(column = 0, row = 3, sticky = W, pady = 2)
-                      
-        elif self.group4 >= 2:
-            group_4 = ['Vanilla Iced Coffee','Nutella Iced Coffee','Colf Brew Coffee', 'Cafe Au Lait']
-            rec = Label(self.newWindow, text = random.choice(group_4))
-            rec.grid(column = 2, row = 1, sticky = W, pady = 2)
-            thank_you = Button(self.newWindow, text = "Thank you!", command = self.newWindow.destroy)
-            thank_you.grid(column = 2, row = 2, sticky = W, pady = 2)
-            try_again = Button(self.window, text = "Try again", command = self.newPage)
-            try_again.grid(column = 0, row = 2, sticky = W, pady = 2)
-            good_bye = Button(self.window, text = "Goodbye", command = self.window.destroy)
-            good_bye.grid(column = 0, row = 3, sticky = W, pady = 2)
-            good_bye.grid(column = 0, row = 3, sticky = W, pady = 2)
-                       
-        else:
-            generic_coffees = ['Flat White', 'Espresso','Espresso Machiatto', 'Cold Brew Coffee', 'Cafe Au Lait', 'Iced Latte', 'Turkish Coffee', 'Americano', 'Black Coffee']
-            rec = Label(self.newWindow, text = random.choice(generic_coffees))
-            rec.grid(column = 2, row = 1, sticky = W, pady = 2)
-            thank_you = Button(self.newWindow, text = "Thank you!", command = self.newWindow.destroy)
-            thank_you.grid(column = 2, row = 2, sticky = W, pady = 2)
-            try_again = Button(self.window, text = "Try again", command = self.newPage)
-            try_again.grid(column = 0, row = 2, sticky = W, pady = 2)
-            good_bye = Button(self.window, text = "Goodbye", command = self.window.destroy)
-            good_bye.grid(column = 0, row = 3, sticky = W, pady = 2)
+        if self.group4 >= 3:
+            self.result = self.g4
+    def submit(self):
+        if self.group1 < 3:
+            if self.group2 < 3:
+                if self.group3 < 3:
+                    if self.group4 < 3:
+                        self.result = self.rand_coffees
+        elif self.group2 < 3:
+            if self.group1 < 3:
+                if self.group3 < 3:
+                    if self.group4 < 3:
+                        self.result = self.rand_coffees
         
+        elif self.group3 < 3:
+            if self.group2 < 3:
+                if self.group1 < 3:
+                    if self.group4 < 3:
+                        self.result = self.rand_coffees
+        elif self.group4 < 4:
+            if self.group2 < 3:
+                if self.group3 < 3:
+                    if self.group1 < 3:
+                        self.result = self.rand_coffees
+        sgn = Label(self.newWindow,text = random.choice(self.result))
+        sgn.grid(column = 0, row = 8, sticky = W, pady = 2)
+        try_again = Button(self.newWindow, text = "Try again", command = self.newPage)
+        try_again.grid(column = 0, row = 9, sticky = W, pady = 2)
+        good_bye = Button(self.newWindow, text = "Goodbye", command = self.newWindow.destroy)
+        good_bye.grid(column = 0, row = 10, sticky = W, pady = 2)
+        good_bye2 = Button(self.window, text = "Goodbye",command = self.window.destroy)
+        good_bye2.grid(column = 0, row = 1, sticky = W, pady = 2)
 app = NewApp
 app()
+
